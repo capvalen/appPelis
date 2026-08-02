049c02dca2a5e3753dc20af4f00785759249241c
capvalen <infocat2.0@gmail.com>
2026-08-01 20:34:33 -0500

    0.01 primer crud

---
+++ insert_peliculas.sql
@@ -0,0 +1,9 @@
+INSERT INTO is_peliculas (pelicula, visto, dato, nota, clase) VALUES
+('Alien Earth','Falta ver','','0','serie'),
+('Stargate Sg1','Viendo','','5','serie'),
+('Buffy la cazavampiros','Visto','','5','serie'),
+('exorcista (gemelas, la serie)','Falta ver','','0','serie'),
+('la tierra de noche','Falta ver','','0','serie'),
+('el caballero de los 7 reinos','Visto','t1','4','serie'),
+('el joven sherlok','Falta ver','','0','serie'),
+('chernobil','Falta ver','','0','serie');
