41284b76dd2fd5444665a3534c2e61f56fc4b93a
capvalen <infocat2.0@gmail.com>
2026-08-15 11:40:50 -0500

    0.4c vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: 'index.html',
